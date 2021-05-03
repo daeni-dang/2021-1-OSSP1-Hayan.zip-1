304efefbf70359b74b2b5e5814fae56dd78c37ad
eunseo0104 <dyuren0104@naver.com>
2021-05-03 11:38:05 +0900

    Eunseo: get input string [script]

--- hayanzip/app/views.py
+++ hayanzip/app/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def main(request):
+    if request.method == 'POST':
+        text = request.POST['inputStr']
+        print(text)
+        return render(request, 'app/main.html', {'text': text})
     return render(request,'app/main.html')
