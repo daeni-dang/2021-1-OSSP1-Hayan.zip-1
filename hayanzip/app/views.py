19f7edc750a4f0dae00c4ea9e75fe435d043eff7
eunseo0104 <bnsy33@naver.com>
2021-05-07 23:52:38 +0900

    Eunseo: 중복 함수 삭제 후 삭제한 함수 사용된 곳 수정

--- hayanzip/app/views.py
+++ hayanzip/app/views.py
@@ -48,19 +48,6 @@ def is_mark(token):  # 문장부호(. ? ! , · / : )인지 판단
     return False
 
 
-def is_type_of_V(token):
-    if token[1].find('V') != -1:
-        return True
-    else:
-        return False
-
-
-def is_NNG(token):
-    if token[1].find('NNG') != -1:
-        return True
-    else:
-        return False
-
 def sentence_without_part(input_string, string_table):
     #형태소 구분 없이 문자열로만 string 문장 구분
     sentences=[]    #구분된 문장들 담을 배열
@@ -114,13 +101,13 @@ def find_verb(string_table):
     for i in range(0, len(string_table)):
         start_flag = -1  # 서술어의 시작 플래그 초기화
         for j in range(0, len(string_table[i])):
-            if start_flag == -1 and is_type_of_V(string_table[i][j]):  # 문장 요소에 V가 있다면
+            if start_flag == -1 and is_have_tag('V', string_table[i][j]):  # 문장 요소에 V가 있다면
                 start_flag = j  # 시작 플래그는 현재 토큰 index
-            elif is_NNG(string_table[i][j]):  # 문장 요소에 N이 있다면
+            elif is_have_tag('NNG', string_table[i][j]):  # 문장 요소에 N이 있다면
                 start_flag = -1  # 서술어가 아니므로 start_flag = -1
         if start_flag != -1:  # start_flag가 -1이면 서술어가 없다는 것
             if start_flag > 0:  # start flag가 0이라면 앞에 것 볼 필요X
-                if is_NNG(string_table[i][start_flag - 1]):  # start_flag 앞의 토큰이 명사라면
+                if is_have_tag('NNG', string_table[i][start_flag - 1]):  # start_flag 앞의 토큰이 명사라면
                     start_flag -= 1  # 시작 플래그를 하나 줄인다.
             print('V:', end=' ')
             for k in range(start_flag, len(string_table[i])):  # start_flag부터 끝까지 서술어
