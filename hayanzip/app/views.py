909a10780df2525c1cc25ddb848ec67f6298e78d
eunseo0104 <dyuren0104@naver.com>
2021-05-07 20:54:29 +0900

    Eunseo: add predicate find

--- hayanzip/app/views.py
+++ hayanzip/app/views.py
@@ -6,12 +6,12 @@ from eunjeon import Mecab
 def main(request):
     if request.method == 'POST':
         str = request.POST.get('final_str', None)
-        if str == None :
+        if str == None :    #대본 입력됐을 경우
             text = request.POST['inputStr']
             sentence_division(text)
             return render(request, 'app/main.html', {'text': text})
         else:
-            sentence_division(str)
+            sentence_division(str) #음성인식 됐을 경우
     return render(request,'app/main.html')
 
 def add_space_after_mot(input_string):      # '못' 뒤에 띄어쓰기 추가하는 함수 : '못'을 기준으로 split한 후, 각 요소 사이에 '못+공백'을 추가하여 합침.
@@ -42,6 +42,18 @@ def is_mark(token):          # 문장부호(. ? ! , · / : )인지 판단
         return True
     return False
 
+def is_type_of_V(token):
+    if token[1].find('V') != -1 :
+        return True
+    else:
+        return False
+
+def is_NNG(token):
+    if token[1].find('NNG') != -1 :
+        return True
+    else:
+        return False
+
 def sentence_division(input_string):
     mecab = Mecab()
 
@@ -65,5 +77,24 @@ def sentence_division(input_string):
 
     for i in range(len(string_table)):  # 테이블 출력
         print(string_table[i])
+    find_verb(string_table)
 
     # 문제! : 다은이 -> 다(MAG) + 은이(NNG) : MAG 삭제
+
+
+def find_verb(string_table):
+    for i in range(0,len(string_table)):
+        start_flag = -1  # 서술어의 시작 플래그 초기화
+        for j in range(0,len(string_table[i])):
+            if start_flag== -1 and is_type_of_V(string_table[i][j]): #문장 요소에 V가 있다면
+                start_flag= j # 시작 플래그는 현재 토큰 index
+            elif is_NNG(string_table[i][j]): #문장 요소에 N이 있다면
+                start_flag= -1  #서술어가 아니므로 start_flag = -1
+        if start_flag!=-1:  #start_flag가 -1이면 서술어가 없다는 것
+            if start_flag>0: #start flag가 0이라면 앞에 것 볼 필요X
+                if is_NNG(string_table[i][start_flag-1]): #start_flag 앞의 토큰이 명사라면
+                    start_flag-=1 #시작 플래그를 하나 줄인다.
+            print('V:', end=' ')
+            for k in range(start_flag, len(string_table[i])): #start_flag부터 끝까지 서술어
+                print(string_table[i][k], end=' ')
+            print()
